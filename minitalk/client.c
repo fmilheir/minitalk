f72258522ecef441c735b3ff6c195992a7729684
fmilheir <contada42@gamil.com>
2022-06-24 17:00:54 +0100

    all

--- minitalk/client.c
+++ minitalk/client.c
@@ -7,19 +7,63 @@ void    message(void)
     write(1, "usage: ./client [server-pid] [message]\n", 39);
 	exit(0);
 }
-void	success(int sig)
+
+int	send_bit(int pid, char *str)
 {
-	(void)sig;
-	write(1, "Data has been received.\n", 25);
+	static char	*message = 0;
+	static int	s_pid = 0;
+	static int	bits = -1;
+
+	if (str)
+		message = ft_strdup(str);
+	if (!message)
+		error(0);
+	if (pid)
+		s_pid = pid;
+	if (message[++bits / 8])
+	{
+		if (message[bits / 8] & (0x80 >> (bits % 8)))
+		{
+			if (kill(s_pid, SIGUSR2) == -1)
+				error(message);
+		}
+		else if (kill(s_pid, SIGUSR1) == -1)
+			error(message);
+		return (0);
+	}
+	if (!send_null(s_pid, message))
+		return (0);
+	free(message);
+	return (1);
 }
 
+void	handler_sigusr(int signum)
+{
+	int	end;
 
+	end = 0;
+	if (signum == SIGUSR1)
+		end = send_bit(0, 0);
+	else if (signum == SIGUSR2)
+	{
+		write(1, "An Error ocured!\n", 17);
+		exit(EXIT_FAILURE);
+	}
+	if (end)
+	{
+		write(1, "client: operation successful.\n", 30);
+		exit(EXIT_SUCCESS);
+	}
+}
 
 int main(int argc, char **argv)
 {
     if (argc != 3)
         message()
-    signal(SIGUSR1, success);
-	handler(argv[1], argv[2]);
+    signal(SIGUSR1, handler_sigusr);
+	signal(SIGUSR2, handler_sigusr);
+	send_bit(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,7 +3,7 @@
 #include "minitalk.h"
 
 
-// aqui converto o  int da funcao getpid numa string usango o itoa para poderr contar co  o strlen o tamanho para por no write
+// aqui converto o  int da funcao getpid numa string usando o itoa para poder contar com  o strlen o tamanho para por no write
 void		print_pid(void)
 {
 	char	*tmp_pid;
@@ -13,15 +13,47 @@ void		print_pid(void)
 	free(tmp_pid);
 }
 
-void main(void)
+
+void	handler_sigusr(int signum, siginfo_t *info, void *context)
+{
+	static char	c = 0xFF;
+	static int	bits = 0;
+	static int	pid = 0;
+	static char	*message = 0;
+
+	(void)context;
+	if (info->si_pid)
+		pid = info->si_pid;
+	if (signum == SIGUSR1)
+		c ^= 0x80 >> bits;
+	else if (signum == SIGUSR2)
+		c |= 0x80 >> bits;
+	if (++bits == 8)
+	{
+		if (c)
+			message = ft_straddc(message, c);
+		else
+			message = print_string(message);
+		bits = 0;
+		c = 0xFF;
+	}
+	if (kill(pid, SIGUSR1) == -1)
+		error(pid, message);
+}
+}
+
+void    main(void)
 {
     struct sigaction	s_sigaction;
 
     print_pid();
-    s_sigaction.sa_sigaction = action;
 	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
+	my_sa.sa_sigaction = my_handler;
+	/*sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);*/
+	sigaction(SIGUSR1, &s_sigaction, NULL);
+	sigaction(SIGUSR2, &s_sigaction, NULL);
 	while (1)
 		pause();
 	return (0);
